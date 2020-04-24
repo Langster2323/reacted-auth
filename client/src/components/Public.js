8456079b202f3eb445c759fc463f66290416814d
Langster2323 <langahkeem@gmail.com>
2020-04-24 10:20:02 -0500

    Home page for health curve

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -4,9 +4,9 @@ export default class Courses extends Component {
   render() {
     return (
       <div className="bounds">
-        <div className="grid-100">
-          <h1>Welcome to the Main Page</h1>
-        </div>
+          <div className="element">
+            <h1 className="circle-txt">Health Curve</h1>
+          </div>
       </div>
     );
   }
